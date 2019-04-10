68840346f2f5e5f5a7dd53abc42e60cc2c7d743e
batsm <mikewheeler360@gmail.com>
2019-04-10 01:51:54 +0100

    Updated login and create account themes plus added some functionality to those pages

--- app/src/main/java/advancedmessagingapp/com/CreateAccount.kt
+++ app/src/main/java/advancedmessagingapp/com/CreateAccount.kt
@@ -1,12 +1,20 @@
 package advancedmessagingapp.com
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_create_account.*
 
-class CreateAccount : AppCompatActivity() {
+class CreateAccount : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        btnSignIn.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/advancedmessagingapp/com/LoginActivity.kt
+++ app/src/main/java/advancedmessagingapp/com/LoginActivity.kt
@@ -1,13 +1,26 @@
 package advancedmessagingapp.com
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.FragmentActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnNewAccount.setOnClickListener {
+            var intent = Intent(this, CreateAccount::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun showMessage(view: View, message: String) {
+        Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE).setAction("Action", null).show()
     }
 }
