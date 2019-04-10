ce1b9b8d2181a5824c75044e8c08bce38551648c
batsm <mikewheeler360@gmail.com>
2019-04-10 01:25:10 +0100

    Create account page created and designed

---
+++ app/src/main/java/advancedmessagingapp/com/CreateAccount.kt
@@ -0,0 +1,12 @@
+package advancedmessagingapp.com
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccount : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
