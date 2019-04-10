e64d0f63f29495924731aa6e20d678804669ea5b
batsm <mikewheeler360@gmail.com>
2019-04-10 01:02:08 +0100

    Login activity deign finished

--- app/src/main/java/advancedmessagingapp/com/LoginActivity.kt
+++ app/src/main/java/advancedmessagingapp/com/LoginActivity.kt
@@ -2,8 +2,9 @@ package advancedmessagingapp.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
