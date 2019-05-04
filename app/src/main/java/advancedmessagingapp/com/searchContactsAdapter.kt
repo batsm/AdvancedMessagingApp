42bac2dbf9eea6000fdbb66fe0ce4043d1072643
batsm <mikewheeler360@gmail.com>
2019-05-04 18:08:01 +0100

    Created search contacts recycler view

---
+++ app/src/main/java/advancedmessagingapp/com/searchContactsAdapter.kt
@@ -0,0 +1,18 @@
+package advancedmessagingapp.com
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class searchContactsAdapter (private val searchedContactList: ArrayList<searchedContactContainerData>): RecyclerView.Adapter<searchContactsAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): searchContactsAdapter.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: searchContactsAdapter.ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
